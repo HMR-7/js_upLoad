959207a4f29651c36ca56d148f7db25be3759484
黄茂瑞 <844173921@qq.com>
2020-08-05 11:09:43 +0800

    08-05

--- serve.js
+++ serve.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const fs = require('fs');
 var app = express();
-//接收前台POST过来的base64
-var imgData = ''
 //过滤data:URL
 app.get('/up', function (req, res) {
-    console.log(req.query);
+    // console.log(req.query);
     let imgData = req.query.url
     var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
     var dataBuffer = new Buffer(base64Data, 'base64'); // 解码图片
